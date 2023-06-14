3299938eaa43970c7ae8779328c04dd19a879580
Michael Bianco <iloveitaly@gmail.com>
2023-06-13 20:56:07 -0600

    adding expand url export

--- lib.js
+++ lib.js
@@ -2,4 +2,22 @@
 
 const followRedirect = require("./follow-redirect-url");
 
+exports.expandUrl = async function(url) {
+  let resolvedUrlList;
+
+  resolvedUrlList = await followRedirect.startFollowing(url, {
+    ignoreSslErrors: true,
+  });
+
+  const lastUrlHop = resolvedUrlList[resolvedUrlList.length - 1];
+
+  if(lastUrlHop.error) {
+    return [false, lastUrlHop]
+  }
+
+  const resolvedUrl = resolvedUrlList[resolvedUrlList.length - 1].url;
+
+  return [true, resolvedUrl];
+}
+
 exports.startFollowing = followRedirect.startFollowing;
